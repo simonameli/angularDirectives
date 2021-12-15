60f40723a78da75e48ae4b902fe063f20eff0235
simonameli <simona.meli21@gmail.com>
2021-12-15 11:42:51 +0100

    con href invece di metodo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
- viewMode='map';
+ viewMode=['map'];
 
  changeViewMode(viewMode){
 
