929c3582d6ce3d3ce2100c8d5bc5b31d643166aa
simonameli <simona.meli21@gmail.com>
2021-12-15 10:59:23 +0100

    pills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+ viewMode='map';
+
+ changeViewMode(viewMode){
+
+     
+ }
 }
